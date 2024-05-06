a60ce0a2ba4952781462d8963ae095c2e171862c
Tomás Peyronel <tpeyronel@gmail.com>
2024-05-06 16:37:13 -0300

    Implement TransportTx with selective repeat
    Co-authored-by: Tomas Achaval <achaval-tomas@users.noreply.github.com>
    Co-authored-by: Tomás Maraschio <tomimara52@users.noreply.github.com>

--- TransportTx.cc
+++ TransportTx.cc
@@ -1,25 +1,41 @@
 #ifndef TRANSPORT_TX
 #define TRANSPORT_TX
 
+#include "DataPkt_m.h"
+#include "FeedbackPkt_m.h"
+#include "TimeoutMsg_m.h"
+#include <algorithm>
+#include <deque>
 #include <omnetpp.h>
 #include <string.h>
 
 enum PacketKind {
     Data,
-    Feedback
+    Feedback,
+    Timeout,
 };
 
-class FeedbackPkt : omnetpp::cPacket {
-    
+enum PacketStatus {
+    Ready,
+    Sent,
+    Acked,
+};
+
+struct DataPktWithStatus {
+    DataPkt* pkt;
+    PacketStatus status;
 };
 
 using namespace omnetpp;
 
 class TransportTx : public cSimpleModule {
 private:
-    cQueue buffer;
+    std::deque<DataPktWithStatus> buffer;
     cMessage* endServiceEvent = NULL;
     simtime_t serviceTime;
+    unsigned int windowStart = 0;
+    unsigned int windowSize = 0;
+    unsigned int inFlightPackets = 0;
 
 public:
     TransportTx();
@@ -29,6 +45,13 @@ protected:
     void initialize() override;
     void finish() override;
     void handleMessage(cMessage* msg) override;
+
+private:
+    void handleEndServiceMessage(cMessage* msg);
+    void handleDataPacket(DataPkt* pkt);
+    void handleFeedbackPacket(FeedbackPkt* pkt);
+    void trySlideWindow();
+    void handleTimeoutMessage(TimeoutMsg* msg);
 };
 Define_Module(TransportTx);
 
@@ -40,49 +63,112 @@ TransportTx::~TransportTx() {
 }
 
 void TransportTx::initialize() {
-    buffer.setName("buffer");
     endServiceEvent = new cMessage("endService");
+    scheduleAt(simTime(), endServiceEvent);
 }
 
 void TransportTx::finish() {
 }
 
 void TransportTx::handleMessage(cMessage* msg) {
-    // if msg is signaling an endServiceEvent
     if (msg == endServiceEvent) {
-        // if packet in buffer, send next one
-        if (!buffer.isEmpty()) {
-            // dequeue packet
-            cPacket* pkt = dynamic_cast<cPacket*>(buffer.pop());
-            // send packet
-            send(pkt, "toOut$o");
-            // start new service
-            serviceTime = pkt->getDuration();
-            scheduleAt(simTime() + serviceTime, endServiceEvent);
-        }
+        handleEndServiceMessage(msg);
     } else if (msg->getKind() == PacketKind::Data) {
-        if (buffer.getLength() >= par("bufferSize").intValue()) {
-            delete msg; // drop msg
-            this->bubble("packet dropped");
-        } else { // if msg is a data packet
-            // enqueue the packet
-            buffer.insert(msg);
-            // if the server is idle
-            if (!endServiceEvent->isScheduled()) {
-                // start the service
-                scheduleAt(simTime(), endServiceEvent);
-            }
-        }
+        handleDataPacket(dynamic_cast<DataPkt*>(msg));
     } else if (msg->getKind() == PacketKind::Feedback) {
-        FeedbackPkt* pkt = dynamic_cast<FeedbackPkt*>(msg);
-
+        handleFeedbackPacket(dynamic_cast<FeedbackPkt*>(msg));
+    } else if (msg->getKind() == PacketKind::Timeout) {
+        handleTimeoutMessage(dynamic_cast<TimeoutMsg*>(msg));
     }
 }
 
-/* Nuestro plan de control de flujo (REPETICION SELECTIVA):
- *  wStart = r
- *  wSize = N
- * 
- */
+void TransportTx::handleEndServiceMessage(cMessage* msg) {
+    if (inFlightPackets >= windowSize) {
+        return;
+    }
+
+    auto pktToSend = std::find_if(buffer.begin(), buffer.end(), [](auto p) { return p.status == PacketStatus::Ready; });
+    if (pktToSend == buffer.end()) {
+        return;
+    }
+
+    send(pktToSend->pkt, "toOut$o");
+    pktToSend->status = PacketStatus::Sent;
+
+    serviceTime = pktToSend->pkt->getDuration();
+    // TODO: make sure that omnet always delivers scheduled self-messages.
+    scheduleAt(simTime() + serviceTime, endServiceEvent);
+}
+
+// From Generator
+void TransportTx::handleDataPacket(DataPkt* pkt) {
+    if (buffer.size() >= par("bufferSize").intValue()) {
+        delete pkt;
+        this->bubble("packet dropped");
+    } else {
+        auto seqNumber = windowStart + buffer.size();
+        pkt->setSeqNumber(seqNumber);
+
+        auto timeoutMsg = new TimeoutMsg("timeout", PacketKind::Timeout);
+        scheduleAt(simTime() + par("timeoutTime"), timeoutMsg);
+
+        buffer.push_back(DataPktWithStatus { pkt, PacketStatus::Ready });
+    }
+}
+
+// From Receiver
+void TransportTx::handleFeedbackPacket(FeedbackPkt* pkt) {
+    auto ackNumber = pkt->getAckNumber();
+    if (ackNumber < windowStart) {
+        return;
+    }
+
+    auto pktIdx = ackNumber - windowStart;
+    if (pktIdx >= windowSize || pktIdx >= buffer.size()) {
+        std::cerr << "pktIdx out of bounds (feedback packet)" << std::endl;
+        return;
+    }
+
+    // MAYBE: windowSize = pkt->getWindowSize();
+    buffer[pktIdx].status = PacketStatus::Acked;
+
+    trySlideWindow();
+}
+
+void TransportTx::trySlideWindow() {
+    auto leadingAckedCount = 0;
+    for (auto pkt : buffer) {
+        if (pkt.status != PacketStatus::Acked) {
+            break;
+        }
+        leadingAckedCount += 1;
+    }
+
+    for (auto i = 0; i < leadingAckedCount; ++i) {
+        buffer.pop_front();
+    }
+
+    windowStart += leadingAckedCount;
+}
+
+void TransportTx::handleTimeoutMessage(TimeoutMsg* msg) {
+    auto seqNumber = msg->getSeqNumber();
+    if (seqNumber < windowStart) {
+        return;
+    }
+
+    auto pktIdx = seqNumber - windowStart;
+    if (pktIdx >= windowSize || pktIdx >= buffer.size()) {
+        std::cerr << "pktIdx out of bounds (timeout message)" << std::endl;
+        return;
+    }
+
+    auto packet = &buffer[pktIdx];
+    if (packet->status != PacketStatus::Acked) {
+        packet->status = PacketStatus::Ready;
+    }
+
+    delete msg;
+}
 
 #endif /* TRANSPORT_TX */
